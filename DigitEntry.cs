130d51134b298610f5584d175d8439318b7337b6
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2020-02-27 17:10:47 -0600

    Add project files.

---
+++ DigitEntry.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitClassifierWithErrorVisualization
+{
+    class DigitEntry
+    {
+        // STATIC VARIABLES-------------------------------------------------------------------
+        // 0-inclusive number of possible classes
+        static readonly int NUM_POSSIBLE_DIGITS = 4;
+
+        // Maximum of integer values used to represent each pixel
+        static readonly int MAX_PIX_VALUE = 16;
+
+        // Number of pixel values that represent the digit
+        static readonly int NUM_PIX_VALUES = 64;
+
+        // Desired output for incorrect classes
+        static readonly float DESIRED_INCORRECT = 0.1f;
+
+        // Desired output for correct classes
+        static readonly float DESIRED_CORRECT = 0.9f;
+
+        // PRIVATE VARIABLES------------------------------------------------------------------
+        // Values that represent the data. Should be normalized and have a size of 64.
+        private List<float> dataValues;
+
+        // Desired output vector of classifier where the index of the maximum value . Should
+        // have a size of NUM_POSSIBLE_DIGITS.
+        private List<float> desiredOutputs;
+
+        // Actual class of the digit
+        private readonly int actualClass;
+
+        // GETTERS----------------------------------------------------------------------------
+        public ref List<float> getDataValues() { return ref dataValues; }
+        public ref List<float> getDesiredOutputs() { return ref desiredOutputs; }
+
+        // CONSTRUCTORS-----------------------------------------------------------------------
+        public DigitEntry(ref List<float> rawData)
+        {
+            // Set the actual class value
+            actualClass = (int)rawData[rawData.Count - 1];
+
+            // Remove the class from the raw data to make it only the pixel data
+            rawData.RemoveAt(rawData.Count - 1);
+
+            // Normalize and set the pixel data values
+            normalizeAndSetDataValues(ref rawData);
+
+            // Define the desired output list
+            setDesiredOutputs();
+        }
+
+        // HELPERS----------------------------------------------------------------------------
+        // Normalize the data and assign the normalized values to the dataValues list.
+        private void normalizeAndSetDataValues(ref List<float> digitData)
+        {
+            foreach (float value in digitData)
+            {
+                dataValues.Add(value / (float)MAX_PIX_VALUE);
+            }
+        }
+
+        // Define the desired output list where the index of the maximum value of the list
+        // represents the actual digit classification.
+        private void setDesiredOutputs()
+        {
+            // Define the list type
+            desiredOutputs = new List<float>(NUM_POSSIBLE_DIGITS);
+
+            // Initialize all values to 0.1
+            for (int i = 0; i < desiredOutputs.Count; ++i)
+            {
+                desiredOutputs[i] = i == actualClass ? DESIRED_CORRECT : DESIRED_INCORRECT;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitClassifierWithErrorVisualization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Global variables----------------------------------------------------------
+        // Filenames of the files that contain the test and training data
+        const string TEST_FILENAME = "optdigits-3.tes";
+        const string TRAIN_FILENAME = "optdigits-3.tra";
+
+        // Datasets to be used loaded into memory
+        List<DigitEntry> testDigitEntries;
+        List<DigitEntry> trainDigitEntries;
+
+        public void collectData()
+        {
+            FileStream testFile = File.OpenRead(TEST_FILENAME);
+            FileStream trainFile = File.OpenRead(TRAIN_FILENAME);
+
+            List<float> rawTestData = parseData(ref testFile);
+        }
+
+        public List<float> parseData(ref FileStream inFile)
+        {
+            string line = "";
+            string current = "";
+            List<float> rawData = new List<float>();
+
+            using (StreamReader streamReader = new StreamReader(inFile))
+            {
+                while (line != null)
+                {
+                    line = streamReader.ReadLine();
+                    foreach (char ch in line)
+                    {
+                        if (ch != ',' && ch != '\n') current += ch;
+                        else
+                        {
+                            rawData.Add((float)Convert.ToDouble(current));
+                            current = "";
+                        }
+                    }
+                }
+            }
+
+            return rawData;
+        }
+    }
+}
