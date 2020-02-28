59aee10bbabbc326d673348f81aa5971fcc5e37d
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2020-02-28 13:20:06 -0600

    Started work on abstract matrix operations class to help with stuff.

---
+++ MatrixOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitClassifierWithErrorVisualization
+{
+    abstract class MatrixOperations<T>
+    {
+        static List<T> Multiply(List<List<T>> first, List<T> second)
+        {
+            int numRowsInFirst = first.Count;
+            int numColsInFirst = first[0].Count;
+
+            int numRowsInSecond = 1;
+            int numColsInSecond = second.Count;
+
+            List<T> result = new List<T>();
+
+            if (numColsInFirst != numColsInSecond)
+            {
+                result = null;
+                return result;
+            }
+            else
+            {
+               // TODO: Implement matrix multiplication
+            }
+        }
+    }
+}
--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -8,5 +8,6 @@ namespace DigitClassifierWithErrorVisualization
 {
     class NeuralNetwork
     {
+
     }
 }
